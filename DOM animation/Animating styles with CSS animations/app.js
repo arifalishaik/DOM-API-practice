c213646f45904b67afca98abbc0ec8367de09cb3
arifalishaik <arifali2016@gmail.com>
2018-03-28 15:37:01 +0530

    Animating styles with CSS animations

---
+++ DOM animation/Animating styles with CSS animations/app.js
@@ -0,0 +1,32 @@
+var infoDiv = document.getElementById("info");
+
+var countdown = document.getElementById("countdown");
+var countItDown = function () {
+    var currentTime = parseFloat( countdown.textContent );
+    if ( currentTime > 0 ) {
+        countdown.textContent = currentTime - 1;
+    } else {
+        window.clearInterval( timer );
+    }
+};
+var timer = window.setInterval( countItDown, 1000 );
+
+  /*
+  // Step 1. What element do we want to animate ?
+  var ohnoes = document.getElementById("ohnoes");
+  ohnoes.style.width = "50px";
+  
+  // Step 2. What function will change it each time ?
+  var startTime = new Date().getTime();
+  var makeItBigger = function() {
+      var currTime = new Date().getTime();
+      var newWidth = (50 + ( ( currTime - startTime ) / 1000 ) * 30);
+       ohnoes.style.width = newWidth + "px";
+       
+       if ( newWidth < 300 ) {
+           window.requestAnimationFrame(makeItBigger);
+       }
+    
+  };
+  makeItBigger();
+  */
